8b2b1ac282cc46a5671d8eeb7f89a140e9647394
Felipe Chung <fchung.dev@gmail.com>
2021-02-11 17:58:46 -0600

    edit navbar css

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      <div className="top">
-        <Navbar></Navbar>
-      </div>
-      <div id="footer">This is the footer</div>
+      <Navbar></Navbar>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,44 +4,42 @@ import * as FaIcons from "react-icons/fa";
 
 function Navbar() {
   return (
-    <div>
-      <div className="navbar">
-        <div className="navbar-container">
-          <ul>
-            <li className="nav-item">
-              <a href="#footer" className="nav-links">
-                <FaIcons.FaHome></FaIcons.FaHome>
-                <span>Home</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#about" className="nav-links">
-                <FaIcons.FaUser></FaIcons.FaUser>
-                <span>About</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#portfolio" className="nav-links">
-                <FaIcons.FaLayerGroup></FaIcons.FaLayerGroup>
-                <span>Portfolio</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#contact" className="nav-links">
-                <FaIcons.FaComments></FaIcons.FaComments>
-                <span>Contact</span>
-              </a>
-            </li>
-            <li className="nav-icons">
-              <a href="#linkedin">
-                <FaIcons.FaLinkedinIn></FaIcons.FaLinkedinIn>
-              </a>
-              <a href="#github">
-                <FaIcons.FaGithub></FaIcons.FaGithub>
-              </a>
-            </li>
-          </ul>
-        </div>
+    <div className="navbar">
+      <div className="navbar-container">
+        <ul className="nav-items">
+          <li className="nav-item">
+            <a href="#footer" className="nav-links">
+              <FaIcons.FaHome></FaIcons.FaHome>
+              <span>Home</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#about" className="nav-links">
+              <FaIcons.FaUser></FaIcons.FaUser>
+              <span>About</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#portfolio" className="nav-links">
+              <FaIcons.FaLayerGroup></FaIcons.FaLayerGroup>
+              <span>Portfolio</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#contact" className="nav-links">
+              <FaIcons.FaComments></FaIcons.FaComments>
+              <span>Contact</span>
+            </a>
+          </li>
+          <li className="nav-item nav-links">
+            <a href="#linkedin" id="linkedin">
+              <FaIcons.FaLinkedinIn></FaIcons.FaLinkedinIn>
+            </a>
+            <a href="#github">
+              <FaIcons.FaGithub></FaIcons.FaGithub>
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
