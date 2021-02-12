547849be95e55360dfac1f2fcf0b86e77bcd456f
Felipe Chung <fchung.dev@gmail.com>
2021-02-12 14:02:21 -0600

    add main component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Main></Main>
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Main.css";
+
+function Main() {
+  return (
+    <div className="main-container">
+      <h3 className="message">Hello, I'm</h3>
+      <h1>Felipe Chung</h1>
+    </div>
+  );
+}
+
+export default Main;
