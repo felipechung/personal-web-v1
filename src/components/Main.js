0f4774cac5f2eb3b8afab5ffc077d8fe3332ede3
Felipe Chung <fchung.dev@gmail.com>
2021-02-13 15:17:17 -0600

    add background images

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,8 +4,10 @@ import "./Main.css";
 function Main() {
   return (
     <div className="main-container">
-      <h3 className="message">Hello, I'm</h3>
-      <h1>Felipe Chung</h1>
+      <div className="intro">
+        <h3>Hello, I'm</h3>
+        <h1>Felipe Chung</h1>
+      </div>
     </div>
   );
 }
