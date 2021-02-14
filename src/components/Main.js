a39030a1192020965eba4b5f96896f655ae858ce
Felipe Chung <fchung.dev@gmail.com>
2021-02-14 14:55:23 -0600

    add google fonts

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,6 +7,8 @@ function Main() {
       <div className="intro">
         <h3>Hello, I'm</h3>
         <h1>Felipe Chung</h1>
+        <p>a software developer based in New York City,</p>
+        <p> passionate about front-end development</p>
       </div>
     </div>
   );
