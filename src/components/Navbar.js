e54ffac036a7e027c268b484a70537ff8cf51934
Felipe Chung <fchung.dev@gmail.com>
2021-02-14 15:05:42 -0600

    edit css for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,25 +9,25 @@ function Navbar() {
         <ul className="nav-items">
           <li className="nav-item">
             <a href="#footer" className="nav-links">
-              <FaIcons.FaHome></FaIcons.FaHome>
+              <FaIcons.FaHome className="nav-icon"></FaIcons.FaHome>
               <span>Home</span>
             </a>
           </li>
           <li className="nav-item">
             <a href="#about" className="nav-links">
-              <FaIcons.FaUser></FaIcons.FaUser>
+              <FaIcons.FaUser className="nav-icon"></FaIcons.FaUser>
               <span>About</span>
             </a>
           </li>
           <li className="nav-item">
             <a href="#portfolio" className="nav-links">
-              <FaIcons.FaLayerGroup></FaIcons.FaLayerGroup>
+              <FaIcons.FaLayerGroup className="nav-icon"></FaIcons.FaLayerGroup>
               <span>Portfolio</span>
             </a>
           </li>
           <li className="nav-item">
             <a href="#contact" className="nav-links">
-              <FaIcons.FaComments></FaIcons.FaComments>
+              <FaIcons.FaComments className="nav-icon"></FaIcons.FaComments>
               <span>Contact</span>
             </a>
           </li>
