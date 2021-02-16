f3c83fdde9908c22754691025a29bdec4300887f
Felipe Chung <fchung.dev@gmail.com>
2021-02-16 14:53:03 -0600

    add pages folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import Main from "./components/Main/Main";
-import Portfolio from "./components/Portfolio/Portfolio";
+import Main from "./pages/Main/Main";
+import Portfolio from "./pages/Portfolio/Portfolio";
 
 function App() {
   return (
