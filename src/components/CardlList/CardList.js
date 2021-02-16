4f5ab0bbd37d6a6f323e77fe23d317cee313e3ed
Felipe Chung <fchung.dev@gmail.com>
2021-02-16 14:48:50 -0600

    add state on CardList component

--- src/components/CardlList/CardList.js
+++ src/components/CardlList/CardList.js
@@ -1,6 +1,25 @@
 import React, { Component } from "react";
 
 export class CardList extends Component {
+  state = {
+    projects: [
+      {
+        title: "Party Watch",
+        image: "../../images/ptwatch.jpg",
+        id: 1,
+      },
+      {
+        title: "Restaurant Website",
+        image: "",
+        id: 2,
+      },
+      {
+        title: "Release Date",
+        image: "",
+        id: 3,
+      },
+    ],
+  };
   render() {
     return <div></div>;
   }
