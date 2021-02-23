a7dcb64a93bdf6cdfadad961fe8cdb638d911964
Felipe Chung <fchung.dev@gmail.com>
2021-02-23 15:09:47 -0600

    add new navbar css

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,6 +6,7 @@ function Navbar() {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   return (
     <>
       <nav className="navbar">
@@ -15,6 +16,36 @@ function Navbar() {
         <div className="menu-icon" onClick={handleClick}>
           <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
         </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <a className="nav-links" href="#home" onClick={closeMobileMenu}>
+              <FaIcons.FaHome></FaIcons.FaHome>
+              <span>Home</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className="nav-links"
+              href="#portfolio"
+              onClick={closeMobileMenu}
+            >
+              <FaIcons.FaLayerGroup></FaIcons.FaLayerGroup>
+              <span>Portfolio</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-links" href="#about" onClick={closeMobileMenu}>
+              <FaIcons.FaUserAlt></FaIcons.FaUserAlt>
+              <span>About</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-links" href="#contact" onClick={closeMobileMenu}>
+              <FaIcons.FaComments></FaIcons.FaComments>
+              <span>Contact</span>
+            </a>
+          </li>
+        </ul>
       </nav>
     </>
   );
