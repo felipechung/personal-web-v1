57fd28c0156ee4938fba4f14555c19d89b3be67a
Felipe Chung <fchung.dev@gmail.com>
2021-02-25 13:42:27 -0600

    fix h1 tag in main page

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -3,13 +3,14 @@ import "./Main.css";
 
 function Main() {
   return (
-    <div className="main-container" id="home">
-      <div className="intro">
-        {/* <h3>Hello, I'm</h3> */}
-        <h1>
-          Hi, my name is <span className="name">Felipe Chung</span>
-        </h1>
-        <h1>i design and develop things</h1>
+    <div className="background">
+      <div className="main-container" id="home">
+        <div className="intro">
+          <h1>
+            Hi, my name is <span className="name">Felipe Chung</span>
+          </h1>
+          <h1>i design and develop things</h1>
+        </div>
       </div>
     </div>
   );
