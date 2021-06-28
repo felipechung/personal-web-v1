40d363b3b7f7ea4a8c3bf9d28ea8533d196c06ea
Felipe Chung <fchung.dev@gmail.com>
2021-06-28 09:39:00 -0300

    refactor cardlist from class to funtion

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,41 +1,39 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Card from "../Card/Card";
 import "./CardList.css";
 
-class CardList extends Component {
-  state = {
-    projects: [
-      {
-        name: "Party Watch",
-        image: "https://i.ibb.co/LhQcSCP/ptwatch.jpg",
-        url: "https://felipechung.github.io/party-watch/",
-        id: 1,
-      },
-      {
-        name: "Restaurant Website",
-        image: "https://i.ibb.co/tqkmdSK/restaurant.jpg",
-        url: "https://rocky-coast-45390.herokuapp.com/",
-        id: 2,
-      },
-      {
-        name: "Release Date",
-        image: "https://i.ibb.co/KKh034N/movie-app.jpg",
-        url: "https://immense-escarpment-63343.herokuapp.com/",
-        id: 3,
-      },
-      {
-        name: "Motivational Timer",
-        image: "https://i.ibb.co/DLtD4QY/motivational2.jpg",
-        url: "https://felipechung.github.io/motivational-timer/",
-        id: 4,
-      },
-    ],
-  };
+export default function CardList() {
+  const projects = [
+    {
+      name: "Party Watch",
+      image: "https://i.ibb.co/LhQcSCP/ptwatch.jpg",
+      url: "https://felipechung.github.io/party-watch/",
+      id: 1,
+    },
+    {
+      name: "Restaurant Website",
+      image: "https://i.ibb.co/tqkmdSK/restaurant.jpg",
+      url: "https://rocky-coast-45390.herokuapp.com/",
+      id: 2,
+    },
+    {
+      name: "Release Date",
+      image: "https://i.ibb.co/KKh034N/movie-app.jpg",
+      url: "https://immense-escarpment-63343.herokuapp.com/",
+      id: 3,
+    },
+    {
+      name: "Motivational Timer",
+      image: "https://i.ibb.co/DLtD4QY/motivational2.jpg",
+      url: "https://felipechung.github.io/motivational-timer/",
+      id: 4,
+    },
+  ];
 
-  render() {
-    return (
+  return (
+    <div>
       <div className="card-list-container">
-        {this.state.projects.map((project) => {
+        {projects.map((project) => {
           return (
             <Card
               name={project.name}
@@ -45,8 +43,6 @@ class CardList extends Component {
           );
         })}
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default CardList;
