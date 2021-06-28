7ed13682419d3040798756464d041e441ca9b93f
Felipe Chung <fchung.dev@gmail.com>
2021-06-28 14:43:49 -0300

    edit modal component

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -11,24 +11,34 @@ export default function CardList() {
       name: "Party Watch",
       image: "https://i.ibb.co/LhQcSCP/ptwatch.jpg",
       url: "https://felipechung.github.io/party-watch/",
+      description:
+        "A simple landing page for a personal project. A website to watch videos simultaneously with other people.",
+      technologies: ["HTML", "CSS", "Bootstrap"],
       id: 1,
     },
     {
       name: "Restaurant Website",
       image: "https://i.ibb.co/tqkmdSK/restaurant.jpg",
       url: "https://rocky-coast-45390.herokuapp.com/",
+      description: "A fictional restaurant website.",
+      technologies: ["HTML", "CSS", "Bootstrap"],
       id: 2,
     },
     {
       name: "Release Date",
       image: "https://i.ibb.co/KKh034N/movie-app.jpg",
       url: "https://immense-escarpment-63343.herokuapp.com/",
+      description:
+        "A website to get information on upcoming movies and tv shows.",
+      technologies: ["HTML", "CSS", "Bootstrap"],
       id: 3,
     },
     {
       name: "Motivational Timer",
       image: "https://i.ibb.co/DLtD4QY/motivational2.jpg",
       url: "https://felipechung.github.io/motivational-timer/",
+      description: "A Pomodoro timer with motivational audio messages.",
+      technologies: ["HTML", "CSS", "Bootstrap"],
       id: 4,
     },
   ];
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import ReactDom from "react-dom";
 import "./Modal.css";
+import * as FaIcons from "react-icons/fa";
 
 const portalRoot = document.getElementById("portal-root");
 
@@ -20,14 +21,26 @@ export default function Modal({ isOpen, onClickClose, project }) {
   return ReactDom.createPortal(
     <div className="modal-overlay" ref={modalRef} onClick={closeModal}>
       <div className="modal">
-        <button
-          type="button"
-          className="modal-close-button"
-          onClick={onClickClose}
-        >
-          X
-        </button>
-        {project.name}
+        <div className="modal-content">
+          <div className="modal-header">
+            <span className="modal-title">{project.name}</span>
+            <button
+              type="button"
+              className="modal-close-button"
+              onClick={onClickClose}
+            >
+              <FaIcons.FaTimes></FaIcons.FaTimes>
+            </button>
+          </div>
+
+          <img className="modal-img" src={project.image} alt="Project img" />
+          <p>{project.description}</p>
+          <ul className="about-text bullets">
+            {project.technologies.map((tech) => {
+              return <li className="bullet-text">{tech}</li>;
+            })}
+          </ul>
+        </div>
       </div>
     </div>,
     portalRoot
