8929df058a68ce4f1759047fe97752b2c803f50a
Felipe Chung <fchung.dev@gmail.com>
2024-03-28 13:26:51 -0300

    add styles to experience section

--- src/pages/Experience/Experience.jsx
+++ src/pages/Experience/Experience.jsx
@@ -8,26 +8,28 @@ function Experience() {
         <div className="title">
           <h3>Experience </h3>
           <h2>Where I've worked</h2>
-          <p className="job-title">
-            Frontend Developer{" "}
-            <a
-              className="company-name"
-              href="https://b2bit.company/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              @ B2bit Company
-            </a>
-          </p>
-          <p>September 2021 - June 2023</p>
+          <div className="job-title-container">
+            <p className="job-title">
+              Frontend Developer{" "}
+              <a
+                className="company-name"
+                href="https://b2bit.company/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                @ B2bit Company
+              </a>
+            </p>
+            <span>September 2021 - June 2023</span>
+          </div>
 
-          <ul>
+          <ul className="experiences-container">
             <li>
               Developed and deployed responsive websites utilizing React,
               Typescript, Next.js, and Styled Components
             </li>
             <li>
-              Developed engaging mobile applications for both Android and iOS
+              Built engaging mobile applications for both Android and iOS
               platforms utilizing React Native, focusing on enhancing user
               experience
             </li>
